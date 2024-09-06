55cee5b0125f39e0d2722537e7a940352c3f5afe
muirurimukundi <themasalacollective@gmail.com>
2024-09-06 18:22:41 -0400

    Set up the app with the server

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const express = require('express')
-const app = express();
+const app = express()
+const PORT = process.env.PORT || 3500
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
