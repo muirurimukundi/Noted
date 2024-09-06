4ffbbcb87e92c8711ca835c2528426ec8eb9a3d1
muirurimukundi <themasalacollective@gmail.com>
2024-09-06 18:32:52 -0400

    Add a roots file and connect it to express

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 3500
 
 app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
